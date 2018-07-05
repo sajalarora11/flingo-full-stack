5846a1c87ea7237754f8488eaa5e24bda0d425cb
Sajal Arora <sajalarora007@gmail.com>
2018-07-05 16:21:01 +0530

    Stable Version is out now. Fixed some Templating Issues

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 var app = express();
 
+// settinb up view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({extended: true}));
 
+//establishing connection to mySQL
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -28,20 +29,20 @@ connection.connect((err) => {
 //     }
 // });
 
-
+//RESTful routes...
 app.get('/', (req, res) => {
     res.render("home");
 })
 
 var logo = "http://static1.squarespace.com/static/54acbf50e4b0da465dd70992/t/588fa7251b631bc1f8380100/1528705439167/"
-
+// Route to fetch from the db
 app.get('/search', (req, res) => {
     var from = req.query.from;
     var to = req.query.to;
     var date = req.query.departure;
     q1 = "select * from Flights inner join Route on Flights.routeID = Route.routeID inner join Airport on Route.depID = Airport.airportID where Route.depID = ? and Route.arrID = ? and Flights.depDate = ?;"
     connection.query(q1, [from, to, date] ,(err, rows, fields) => {
-        console.log(rows[0].flight_name);
+        console.log("done");
         res.render('partials/flights', {
             data: rows,
             logo:logo
@@ -50,7 +51,7 @@ app.get('/search', (req, res) => {
     });
 })
 
-
+//litsen to port..
 const port = 8000;
 app.listen(port, () => {
     console.log("Server running on "+port);
